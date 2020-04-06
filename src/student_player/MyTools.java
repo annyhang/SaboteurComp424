5c3aa487a94ea1210b469342b8d83723afc00ab0
annyhang <annyhang98@gmail.com>
2020-04-06 17:37:13 -0400

    TODO's setup

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,50 @@
 package student_player;
 
+import java.util.ArrayList;
+
+import boardgame.Move;
+import Saboteur.SaboteurBoardState;
+
+
+
 public class MyTools {
+	
     public static double getSomething() {
         return Math.random();
     }
+    
+    //TODO
+    /**
+     * Selection method
+     * 
+     * @return the most promising move based on existing paths
+     */
+    public Move selection() {
+    	ArrayList<SaboteurMove> allMoves = SaboteurBoardState.getAllLegalMoves();
+    	
+    	return null;
+    }
+    
+    //TODO
+    /**
+     * Expansion
+     * 
+     * @return the tree with an added node at the branch selected
+     */
+    
+    //TODO
+    /**
+     * Simulation
+     * 
+     * @return an utility value from the simulation of the game
+     */
+    
+    //TODO
+    /**
+     * Propagation
+     * 
+     * @return the updated tree
+     */
+    
+    
 }
--- src/student_player/StudentPlayer.java
+++ src/student_player/StudentPlayer.java
@@ -5,8 +5,25 @@ import boardgame.Move;
 import Saboteur.SaboteurPlayer;
 import Saboteur.SaboteurBoardState;
 
+import java.util.ArrayList;
+
 /** A player file submitted by a student. */
 public class StudentPlayer extends SaboteurPlayer {
+	
+	private boolean hasBonus = false;
+	private boolean hasMalus = false;
+	// private boolean isMalused = false;		this exists as a getter: SaboteurBoardState.getNbMalus(int playerNb)
+	
+	private boolean hasMapCard = false;
+	private int heightPathGoal = 7;					//check???
+	private boolean hasDestroyCard = false;
+	
+	private boolean isNuggetFound = false;
+	private int[][] whereIsNugget = {{-1}, {-1}};	//format of board???
+	
+	//TODO
+	//figure out the type of the board
+	private ArrayList<Integer[]> existingPaths = new ArrayList<Integer[]>();
 
     /**
      * You must modify this constructor to return your student number. This is
@@ -14,7 +31,7 @@ public class StudentPlayer extends SaboteurPlayer {
      * associate you with your agent. The constructor should do nothing else.
      */
     public StudentPlayer() {
-        super("xxxxxxxxx");
+        super("260803297");
     }
 
     /**
@@ -23,15 +40,14 @@ public class StudentPlayer extends SaboteurPlayer {
      * make decisions.
      */
     public Move chooseMove(SaboteurBoardState boardState) {
-        // You probably will make separate functions in MyTools.
-        // For example, maybe you'll need to load some pre-processed best opening
-        // strategies...
-        MyTools.getSomething();
 
-        // Is random the best you can do?
-        Move myMove = boardState.getRandomMove();
-
-        // Return your move to be processed by the server.
-        return myMove;
+    	return null;
     }
+    
+    //TODO
+    /**
+     * Create our own version of SaboteurBoardState because we are not allowed
+     * to use the preimplemented one
+     */
+    
 }
