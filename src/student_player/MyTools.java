225b2eea966b00f92cf8bcd7fe12ed59db8c1cac
annyhang <annyhang98@gmail.com>
2020-04-13 23:42:43 -0400

    a big mess really

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -13,6 +13,7 @@ import Saboteur.cardClasses.*;
 public class MyTools {
 	
 	SaboteurBoardState boardState;
+	int[][] objPos = { {12, 3}, {12, 5}, {12, 7} };
 	
 	
 	MyTools(SaboteurBoardState boardState) {
@@ -20,11 +21,48 @@ public class MyTools {
 	}
 
 	
+	
+	
+	
+	/**
+	 * Get the position of the nugget.
+	 * 
+	 * @return the position of the nugget. If it is unknown, it can either be deduced or return {-1, -1}
+	 */
+	public int[] getNugget() {
+		boolean[] hiddenUnmapped = {false, false, false};
+		int hiddenUnmappedCount = 0;
+		int[] nuggetPos = {-1, -1};
+
+		for (int i=0; i<3; i++) {
+			SaboteurTile[][] tileBoard = this.boardState.getHiddenBoard();
+			String idx = tileBoard[ objPos[i][0] * 3 ][ objPos[i][1] * 3 ].getIdx();
+			if (idx.equals("nugget")) {
+				nuggetPos = objPos[0];
+			} 
+			else if (idx.equals("hidden2") || idx.equals("hidden2")) {
+				hiddenUnmapped[i] = true;
+				hiddenUnmappedCount++;
+			}
+		}
+
+		//nugget hasn't been unmapped but we can deduce where it is
+		if (hiddenUnmappedCount == 2) {
+			for (int i=0; i<hiddenUnmapped.length; i++) {
+				if (hiddenUnmapped[i] == false) {
+					nuggetPos = objPos[i];
+					break;
+				}
+			}
+		}
+		return nuggetPos;
+	}
+
     /**
      * Get the distance between the nugget/objectives and the closest path.
      * Here, the closest path is assumed to be a feasible path.
      */
-    public int distanceNuggetPath(int[][] objectivesPos, int[] nuggetPos, int objectivesFound, int[][] myBoard, SaboteurBoardState boardState) {
+    public int distanceNuggetPath(int[] nuggetPos, int objectivesFound, int[][] myBoard, SaboteurBoardState boardState) {
     	SaboteurTile[][] tileBoard = boardState.getHiddenBoard();
     	int shortestDistance = tileBoard.length;
     	
@@ -33,13 +71,13 @@ public class MyTools {
     	//if we know where the nugget is (or can deduce it), the distance should be between the closest path and the nugget
     	if ((nuggPosx != -1 && nuggPosy != -1) || objectivesFound == 1) {
     		if (objectivesFound == 1) {
-    			nuggPosy = objectivesPos[2][0];
-    			nuggPosx = objectivesPos[2][1];
+    			nuggPosy = objPos[2][0];
+    			nuggPosx = objPos[2][1];
     		}
     		for (int i=tileBoard.length-1; i<=0; i++) {
     			for (int j=0; j<tileBoard.length; j++) {
     				if (tileBoard[i][j] != null) {
-    					//if we encouter an objective, we need to check if can add a tile from that objective first
+    					//if we encounter an objective, we need to check if can add a tile from that objective first
     					if (myBoard[i*3][j*3] > 2) {
     						int[][] path12 = {{0,0,0},{0,1,1},{0,0,0}};
     						int[] posDown = {i+1, j};
@@ -65,9 +103,12 @@ public class MyTools {
     			}
     		}
     	}
-
     	return -1;
     }
-
+	
+	
+	 SaboteurBoardState getBoardState() {
+		return this.boardState;
+	}
     
 }
