91b26a0234b00a872e78bb08a1cf4e2bdb416b0b
annyhang <annyhang98@gmail.com>
2020-04-15 01:08:09 -0400

    temparary details added

--- src/Saboteur/RandomSaboteurPlayer.java
+++ src/Saboteur/RandomSaboteurPlayer.java
@@ -24,33 +24,20 @@ public class RandomSaboteurPlayer extends SaboteurPlayer {
     public Move chooseMove(SaboteurBoardState boardState) {
         System.out.println("random player acting as player number: "+boardState.getTurnPlayer());
 
-        
+
     	SaboteurTile[][] hiddenBoard = boardState.getHiddenBoard();
     	this.myBoard = boardState.getHiddenIntBoard().clone();
-    	int objectiveNb = 2;
     	
-    	//replace empty positions to objectives value
-    	for (int i=0; i<hiddenBoard.length; i++) {
-    		for (int j=0; j<hiddenBoard.length; j++) {
-    			if (hiddenBoard[i][j]!=null) {
-	    			if (hiddenBoard[i][j].getIdx().equals("8")) {
-		    			for (int h=0; h<3; h++) {
-		    				for (int k=0; k<3; k++) {
-			    				this.myBoard[i*3+h][j*3+k] = objectiveNb;
-		    				}
-		    			}
-		    			objectiveNb++;
-	    			}
-    			}
-    		}
-    	}
-
         for (int i=0; i<myBoard.length; i++) {
         	for (int j=0; j<myBoard.length; j++) {
         		if (myBoard[i][j] < 0) {
+        			System.out.print(" ");
+        		} 
+        		else if(hiddenBoard[i/3][j/3].getIdx().contentEquals("entrance")) {
+        			System.out.print("o");
+        		}
+        		else {
         			System.out.print(myBoard[i][j]);
-        		} else {
-        			System.out.print(myBoard[i][j]+""+myBoard[i][j]);
         		}
         		
         	}
